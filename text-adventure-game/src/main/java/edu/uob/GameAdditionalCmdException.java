101bd1b9954d1f2206889c19407f16a47a6d36b9
pcypoon <98314291+pcypoon@users.noreply.github.com>
2022-06-22 20:35:20 +0100

    Update GameAdditionalCmdException.java

--- text-adventure-game/src/main/java/edu/uob/GameAdditionalCmdException.java
+++ text-adventure-game/src/main/java/edu/uob/GameAdditionalCmdException.java
@@ -1,1 +1,16 @@
-package edu.uob;import java.io.Serial;public class GameAdditionalCmdException extends Throwable {    @Serial    private static final long serialVersionUID = -6916847369870850001L;    String errorMessage;    public GameAdditionalCmdException(String errorMessage) {        this.errorMessage = errorMessage;    }    public String toString() {        return errorMessage;    }}
+package edu.uob;
+import java.io.Serial;
+
+public class GameAdditionalCmdException extends Throwable {
+    @Serial
+    private static final long serialVersionUID = -6916847369870850001L;
+    String errorMessage;
+
+    public GameAdditionalCmdException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return errorMessage;
+    }
+}
