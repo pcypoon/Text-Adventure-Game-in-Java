bac837f00db13199723a16d3cc16ebfd1eafd7e9
pcypoon <98314291+pcypoon@users.noreply.github.com>
2022-06-22 20:37:07 +0100

    Update GameTokenizeException.java

--- text-adventure-game/src/main/java/edu/uob/tokenizer/GameTokenizeException.java
+++ text-adventure-game/src/main/java/edu/uob/tokenizer/GameTokenizeException.java
@@ -1,1 +1,17 @@
-package edu.uob;import java.io.Serial;public class GameTokenizeException extends Throwable {    @Serial    private static final long serialVersionUID = -1966799517817170397L;    String errorMessage;    public GameTokenizeException(String errorMessage) {        this.errorMessage = errorMessage;    }    public String toString() {        return errorMessage;    }}
+package edu.uob;
+
+import java.io.Serial;
+
+public class GameTokenizeException extends Throwable {
+    @Serial
+    private static final long serialVersionUID = -1966799517817170397L;
+    String errorMessage;
+
+    public GameTokenizeException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return errorMessage;
+    }
+}
