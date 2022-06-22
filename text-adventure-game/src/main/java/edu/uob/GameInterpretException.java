3ca28c0869b58c3069e4624db4a86ad016706b96
pcypoon <98314291+pcypoon@users.noreply.github.com>
2022-06-22 20:36:26 +0100

    Update GameInterpretException.java

--- text-adventure-game/src/main/java/edu/uob/GameInterpretException.java
+++ text-adventure-game/src/main/java/edu/uob/GameInterpretException.java
@@ -1,1 +1,17 @@
-package edu.uob;import java.io.Serial;public class GameInterpretException extends Throwable {    @Serial    private static final long serialVersionUID = -6595161140799436837L;    String errorMessage;    public GameInterpretException(String errorMessage) {        this.errorMessage = errorMessage;    }    public String toString() {        return errorMessage;    }}
+package edu.uob;
+
+import java.io.Serial;
+
+public class GameInterpretException extends Throwable {
+    @Serial
+    private static final long serialVersionUID = -6595161140799436837L;
+    String errorMessage;
+
+    public GameInterpretException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return errorMessage;
+    }
+}
