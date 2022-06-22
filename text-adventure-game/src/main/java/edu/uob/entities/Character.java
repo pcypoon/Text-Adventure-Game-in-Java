a72a938f5b70a45da8d68bd077f6d931d9483470
pcypoon <98314291+pcypoon@users.noreply.github.com>
2022-06-22 20:38:54 +0100

    Update Character.java

--- text-adventure-game/src/main/java/edu/uob/entities/Character.java
+++ text-adventure-game/src/main/java/edu/uob/entities/Character.java
@@ -1,1 +1,8 @@
-package edu.uob;public class Character extends GameEntity{    public Character(String name, String description){        super(name, description);    }}
+package edu.uob;
+public class Character extends GameEntity{
+
+    public Character(String name, String description){
+        super(name, description);
+
+    }
+}
