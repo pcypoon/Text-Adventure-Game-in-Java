141742177bbfcdc08c4dc72df948543f7099723f
pcypoon <98314291+pcypoon@users.noreply.github.com>
2022-06-22 20:37:38 +0100

    Update GameParseException.java

--- text-adventure-game/src/main/java/edu/uob/parser/GameParseException.java
+++ text-adventure-game/src/main/java/edu/uob/parser/GameParseException.java
@@ -1,1 +1,16 @@
-package edu.uob;import java.io.Serial;public class GameParseException extends Throwable {    @Serial    private static final long serialVersionUID = -8540961016088684119L;    String errorMessage;    public GameParseException(String errorMessage) {        this.errorMessage = errorMessage;    }    public String toString() {        return errorMessage;    }}
+package edu.uob;
+import java.io.Serial;
+
+public class GameParseException extends Throwable {
+    @Serial
+    private static final long serialVersionUID = -8540961016088684119L;
+    String errorMessage;
+
+    public GameParseException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return errorMessage;
+    }
+}
